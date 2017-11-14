74e2aaa466f4fc580e8943268fcdf16ccfeee3a7
MeirShimon <33037292+MeirShimon@users.noreply.github.com>
2017-11-14 23:22:57 +0200

    add main

--- mivne-netunim-2/targil3/targil3/main.cpp
+++ mivne-netunim-2/targil3/targil3/main.cpp
@@ -2,9 +2,41 @@
 using namespace std;
 int main()
 {
-	HuffmanTree temp;
-	temp.encode("in.txt", "out.txt");
-	
+	int chioce;
+	string sourceFile, destFile;
+	HuffmanTree tree;
+	do
+	{
+
+	cout << "enter your choise:" << endl;
+	cout << "1 - encode file" << endl;
+	cout << "2 - dcode file" << endl;
+	cout << "3 - exit" << endl;
+	cin >> chioce;
+	try 
+	{
+		switch (chioce)
+		{
+		case 1:cout << "enter the name sourch file and the name of destination file" << endl;
+			cin >> sourceFile >> destFile;
+			tree.encode(sourceFile, destFile);
+			break;
+		case 2:cout << "enter the name sourch file and the name of destination file" << endl;
+			cin >> sourceFile >> destFile;
+			tree.encode(sourceFile, destFile);
+			break;
+		case 3: cout << "Goodbye" << endl;
+			break;
+		default:cout << "ERROR" << endl;
+			break;
+		}
+	}
+	catch (const string exception)
+	{
+		cout << exception << endl;
+	}
+	} while (chioce!=3);
+
 
 
 	return 0;
