e7c036b961511913c772cf56cc96c4b1af002f1d
meir <nthr120@gmail.com>
2019-05-05 22:17:24 +0300

    begin create login activity

--- sadna-java/project/GetTaxi2/app/src/main/java/il/co/meir_itzik/gettaxi2/MainActivity.java
+++ sadna-java/project/GetTaxi2/app/src/main/java/il/co/meir_itzik/gettaxi2/MainActivity.java
@@ -3,11 +3,26 @@ package il.co.meir_itzik.gettaxi2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class MainActivity extends AppCompatActivity {
+    //FirebaseApp.initializeApp(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        blabla();
+
+
+
+    }
+
+    public void blabla(){
+        FirebaseApp.initializeApp(this);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("meir");
+        myRef.setValue("Hello, World!");
     }
 }
