2721078bdb3a5e369004abe6a94d3a1f1a15f9a3
Itzik Yeret <yeret82088@gmail.com>
2019-09-20 06:16:12 -0700

    build first step of the website

--- web engineering/ex3/app.js
+++ web engineering/ex3/app.js
@@ -1,15 +1,19 @@
 let express = require('express');
 let path = require('path');
 
+let indexRouter = require('./routes/index');
+let aboutRouter = require('./routes/about');
+
 let app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views/partials')));
 
-app.get('/', function(req, res){
-    res.render(`pages/index`)
-});
+app.use('/', indexRouter);
+app.use('/about', aboutRouter);
+
 
 app.listen(3000, function(){
     console.log(`I am listening to port 3000`)
