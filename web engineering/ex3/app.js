ee534d107ada257280d3b3ebad92828d0cf8d1a3
Itzik Yeret <yeret82088@gmail.com>
2019-09-22 03:35:45 -0700

    build first page and about page

--- web engineering/ex3/app.js
+++ web engineering/ex3/app.js
@@ -1,18 +1,26 @@
 let express = require('express');
 let path = require('path');
 
+//require routs
 let indexRouter = require('./routes/index');
 let aboutRouter = require('./routes/about');
+let productsCategory = require('./routes/productsCategory');
+
 
 let app = express();
 
+// set view engine to ejs
 app.set('view engine', 'ejs');
 
+// set folders
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views/partials')));
+app.use(express.static(path.join(__dirname, 'data')));
 
+// set routs
 app.use('/', indexRouter);
 app.use('/about', aboutRouter);
+app.use('/productsCategory', productsCategory);
 
 
 app.listen(3000, function(){
