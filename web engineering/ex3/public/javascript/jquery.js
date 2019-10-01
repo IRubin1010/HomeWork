2a495a075ae336e0107efc83ce247181e0903c57
Itzik Yeret <yeret82088@gmail.com>
2019-10-01 22:52:10 +0300

    complete logout

--- web engineering/ex3/public/javascript/jquery.js
+++ web engineering/ex3/public/javascript/jquery.js
@@ -2,13 +2,27 @@ $(document).ready(async function () {
     let hash = location.hash;
     if (hash !== "") {
         let replacedHash = hash.replace('#', '');
-        if(replacedHash === "stores"){
+        if (replacedHash === "stores") {
             await loadStores();
             return;
         }
         $("#middle-page").load(replacedHash + ".ejs");
 
     }
+});
+
+$(document).ready(function () {
+    $("a[rel~='keep-params']").on("click", function (e) {
+            e.preventDefault();
+            let params = window.location.search;
+            let dest = $(this).attr('href') + params;
+
+            // a short timeout has helped overcome browser bugs
+            window.setTimeout(function () {
+                window.location.href = dest;
+            }, 100);
+        }
+    );
 });
 
 $(document).ready(function () {
@@ -42,6 +56,17 @@ $(document).ready(async function () {
     let productsCategoryJson = await productsCategory.json();
     let template = await jQuery.get('templates/productsCategory.ejs');
     $.tmpl(template, productsCategoryJson).appendTo("#products-category-cards");
+});
+
+$(document).ready(async function () {
+    $('#logout').on("click", function(){
+        let url = window.location.href.toString();
+        if (url.indexOf("?") > 0) {
+            let clean_url = url.substring(0, url.indexOf("?"));
+            window.history.replaceState({}, document.title, clean_url);
+            window.location = clean_url;
+        }
+    })
 });
 
 
--- web engineering/ex3/routes/index.js
+++ web engineering/ex3/routes/index.js
@@ -1,8 +1,23 @@
 let express = require('express');
 let router = express.Router();
+let authService = require('../BL/authService');
 
-router.get('/', function(req, res){
-    res.render(`pages/index`, {page: "firstPage.ejs"})
+router.get('/', function (req, res) {
+
+    let userName = req.query.userName;
+    let password = req.query.password;
+
+    let userRole = undefined;
+
+    if (userName !== undefined && password !== undefined) {
+        let user = authService.getUser(userName, password);
+        if (user !== undefined) {
+            userRole = user.role;
+        }
+    }
+    res.render(`pages/index`, {
+        userRole: userRole
+    })
 });
 
 module.exports = router;
