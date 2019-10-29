90fa11159c844003a9b9c4745ba694493379cf20
meshimon <meshimon@microsoft.com>
2019-10-29 18:37:43 +0200

    Start WPF

---
+++ wpf engineering/InformationKiosk/InformationKiosk.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using InformationKiosk.BL;
+using InformationKiosk.DataProtocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformationKiosk.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var admiService = new AdministratorService();
+            admiService.AddAdministrator("Meir", "Shimon", "M@M", "1234").ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+    }
+}
