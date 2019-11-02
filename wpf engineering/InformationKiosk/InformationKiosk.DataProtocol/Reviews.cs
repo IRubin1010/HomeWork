dac1d016572816f382f037eb46dd3dbada229acb
MeirShimon <nthr120@gmail.com>
2019-11-02 23:19:50 +0200

    add reviews and update score

--- wpf engineering/InformationKiosk/InformationKiosk.DataProtocol/Reviews.cs
+++ wpf engineering/InformationKiosk/InformationKiosk.DataProtocol/Reviews.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,15 @@ namespace InformationKiosk.DataProtocol
 {
     public class Reviews
     {
+        [Key]
+        public int Id { get; set; }
         public string Description { get; set; }
 
         public int Score { get; set; }
 
         //public image Image {get; set; }
+
+        public Guid IceCreamId { get; set; }
+        public IceCream IceCream { get; set; }
     }
 }
